0c146130ef55954b88d194f23e072a832430a8ff
Fauzi <fauzi190198@gmail.com>
2022-06-09 23:40:54 +0700

    feat: assign tugas 7

--- promise.js
+++ promise.js
@@ -1,7 +1,12 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const hasil = await promiseTheaterIXX();
+  const hasil2 = await promiseTheaterVGC();
+  const result = [...hasil, ...hasil2].filter((item) => item.hasil === emotion);
+  return result.length;
+};
 
 module.exports = {
   promiseOutput,
